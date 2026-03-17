ead371cc6157fa8588ee60d3996f8c135a8d0d25
SonoPG <38359416+PGgamer2@users.noreply.github.com>
2026-03-17 21:29:28 +0100

    Fix Mod Menu Integration with Embeddium
    Use reflection when createScreen is not available

--- src/main/java/me/cortex/voxy/client/config/ModMenuIntegration.java
+++ src/main/java/me/cortex/voxy/client/config/ModMenuIntegration.java
@@ -2,18 +2,33 @@ package me.cortex.voxy.client.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import me.cortex.voxy.common.Logger;
 import me.cortex.voxy.commonImpl.VoxyCommon;
 import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
+import net.minecraft.client.gui.screens.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> {
             if (VoxyCommon.isAvailable()) {
-                var screen = (SodiumOptionsGUI) SodiumOptionsGUI.createScreen(parent);
-                //Sorry jelly and douira, please dont hurt me
+				//Sorry jelly and douira, please dont hurt me
+				
+                SodiumOptionsGUI screen = null;
+				if (Arrays.stream(SodiumOptionsGUI.class.getMethods()).anyMatch(method -> method.getName().equals("createScreen"))) {
+					screen = (SodiumOptionsGUI) SodiumOptionsGUI.createScreen(parent);
+				}
+				
                 try {
+					if (screen == null) {
+						// Embeddium support
+						Constructor<SodiumOptionsGUI> constructor = SodiumOptionsGUI.class.getDeclaredConstructor(Screen.class);
+						constructor.setAccessible(true);
+						screen = constructor.newInstance(parent);
+					}
+					
                     //We cant use .setPage() as that invokes rebuildGui, however the screen hasnt been initalized yet
                     // causing things to crash
                     var field = SodiumOptionsGUI.class.getDeclaredField("currentPage");
