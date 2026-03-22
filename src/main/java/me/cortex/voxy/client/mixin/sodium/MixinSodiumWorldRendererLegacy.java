863856b3424657f6f9143687608cb34fd6de87f6
SonoPG <pietro.gabriel.giambanco@gmail.com>
2026-03-22 18:39:10 +0100

    Compat with Sodium 0.5.13 (valk skies embeddium/sodium support)
    This upgrades Sodium to 0.5.13 without abandoning the older versions, making it compatible with Embeddium. Also fixes Mod Menu integration.

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRendererLegacy.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRendererLegacy.java
@@ -1,24 +1,24 @@
 package me.cortex.voxy.client.mixin.sodium;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.cortex.voxy.client.core.IGetVoxyRenderSystem;
+import me.cortex.voxy.client.core.rendering.Viewport;
+import me.cortex.voxy.client.core.util.IrisUtil;
 import me.cortex.voxy.commonImpl.VoxyCommon;
 import me.jellysquid.mods.sodium.client.gl.device.CommandList;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
-
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderMatrices;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderType;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import me.cortex.voxy.client.core.IGetVoxyRenderSystem;
-import me.cortex.voxy.client.core.rendering.Viewport;
-import me.cortex.voxy.client.core.util.IrisUtil;
-import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderMatrices;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderType;
-
 @Mixin(value = SodiumWorldRenderer.class, remap = false)
-public class MixinSodiumWorldRenderer {
+public class MixinSodiumWorldRendererLegacy {
     @Inject(method = "initRenderer", at = @At("TAIL"), remap = false)
     private void voxy$injectThreadUpdate(CommandList cl, CallbackInfo ci) {
         var vi = VoxyCommon.getInstance();
@@ -28,13 +28,15 @@ public class MixinSodiumWorldRenderer {
     @Unique
     private ChunkRenderMatrices voxy$capturedMatrices;
 
-    @Inject(method = "drawChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkRenderMatrices;DDD)V", at = @At("HEAD"))
-    private void voxy$captureMatrices(RenderType renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci) {
-        this.voxy$capturedMatrices = matrices;
+    @Dynamic
+    @Inject(method = "drawChunkLayer(Lnet/minecraft/class_1921;Lnet/minecraft/class_4587;DDD)V", at = @At("HEAD"))
+    private void voxy$captureMatrices(RenderType renderLayer, PoseStack matrixStack, double x, double y, double z, CallbackInfo ci) {
+        this.voxy$capturedMatrices = ChunkRenderMatrices.from(matrixStack);
     }
 
-    @Inject(method = "drawChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkRenderMatrices;DDD)V", at = @At("TAIL"))
-    private void injectRender(RenderType renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci) {
+    @Dynamic
+    @Inject(method = "drawChunkLayer(Lnet/minecraft/class_1921;Lnet/minecraft/class_4587;DDD)V", at = @At("TAIL"))
+    private void injectRender(RenderType renderLayer, PoseStack matrixStack, double x, double y, double z, CallbackInfo ci) {
         this.doRender(this.voxy$capturedMatrices, renderLayer, x, y, z);
     }
     
